b9edf5e22c571614394c546c611219bf43c52e9c
niktos12 <nikitosss121207@gmail.com>
2024-03-04 17:46:02 +0400

    .

--- my-app/src/components/Product.tsx
+++ my-app/src/components/Product.tsx
@@ -33,7 +33,7 @@ export function Product({product} : IProductProps){
           		leaveFrom="transform opacity-100 scale-100"
           		leaveTo="transform opacity-0 scale-95"
 			>
-				<Menu.Items className='absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none'>
+				<Menu.Items className='absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-xl ring-1 ring-black/5 focus:outline-none'>
 					<Menu.Item>
 						{({active})=>(
 							<button className="w-full px-4 py-2 text-lg flex flex-row items-center gap-2" onClick={handleRemoveProduct}>
--- my-app/src/pages/ProductsPage.tsx
+++ my-app/src/pages/ProductsPage.tsx
@@ -9,34 +9,39 @@ export function ProductsPage() {
   const [displayedCount, setDisplayedCount] = useState(5);
   const { modal, open, close } = useContext(ModalAddContext);
   const loadMoreProducts = () => {
-    setDisplayedCount(prevCount => prevCount + 5);
+    setDisplayedCount((prevCount) => prevCount + 5);
   };
   const products = useProductStore((state) => state.products);
   const displayedProducts = products.slice(0, displayedCount);
 
   return (
     <>
-        <div className="grid grid-cols-[repeat(2,450px)] justify-center gap-4 from-black to-slate-950 bg-gradient-to-r h-full content-center">
-      {displayedProducts.map((product) => (
-        <Product key={product.id} product={product} />
-      ))}
-      
-      {modal && (
-        <ModalAdd onClose={close} title="Create product">
-          <CreateProduct />
-        </ModalAdd>
-      )}
-      <button
-        className="bg-green-500 text-white rounded-3xl px-4 py-2 fixed top-5 right-5 z-20"
-        onClick={open}
-      >
-        Create
-      </button>
-    </div>
-    {displayedCount < products.length ? (
-        <button onClick={loadMoreProducts} className="text-2xl font-semibold bg-white rounded-3xl px-4 py-2 w-full text-center mt-4 text-black rounded-3xl">Загрузить еще</button>
-      ) : <p className="text-2xl font-semibold bg-white rounded-3xl px-4 py-2 w-full text-center mt-4 text-black rounded-3xl">Конец списка продуктов</p>}
+      <div className="grid grid-cols-[repeat(2,450px)] justify-center gap-4 content-center h-full ">
+        {displayedProducts.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+
+        {modal && (
+          <ModalAdd onClose={close} title="Create product">
+            <CreateProduct />
+          </ModalAdd>
+        )}
+        <button
+          className="bg-green-500 text-white rounded-3xl px-4 py-2 fixed top-5 right-5 z-20"
+          onClick={open}
+        >
+          Create
+        </button>
+      </div>
+      {displayedProducts.length > 0 &&
+        displayedProducts.length < products.length && (
+          <button
+            onClick={loadMoreProducts}
+            className="text-2xl font-semibold bg-black rounded-3xl px-4 py-2 w-full text-center mt-4 text-white rounded-3xl"
+          >
+            Загрузить еще
+          </button>
+        )}
     </>
-    
   );
 }
